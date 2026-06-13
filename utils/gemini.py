0785bfe5f1f21d1cb0a1e9b57abd5ed093f9f344
HIMANSHUxRAWAT <himanshu.rawat.dev@gmail.com>
2026-06-13 13:51:25 +0530

    Add Streamlit Cloud support

--- utils/gemini.py
+++ utils/gemini.py
@@ -1,18 +1,26 @@
 import os
 
 import google.generativeai as genai
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = None
+
+try:
+    api_key = st.secrets["GEMINI_API_KEY"]
+except Exception:
+    api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
     raise ValueError(
-        "GEMINI_API_KEY not found in .env file"
+        "GEMINI_API_KEY not found."
     )
 
-genai.configure(api_key=api_key)
+genai.configure(
+    api_key=api_key
+)
 
 model = genai.GenerativeModel(
     "gemini-2.5-flash"
